19a105183e675228d68116ddb98fd4a740f488f9
EC2 Default User <ec2-user@ip-172-31-38-242.ap-northeast-1.compute.internal>
2024-08-23 23:32:47 +0000

    [Finish]4-17

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   
   get 'homes/about' => 'homes#about', as: 'about'
   
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
